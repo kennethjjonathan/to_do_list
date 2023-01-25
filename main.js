955c6fd3a8eb50372ec8305b3017f471116fcbbd
kennethjjonathan <106253255+kennethjjonathan@users.noreply.github.com>
2023-01-26 00:49:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,43 @@ const input = document.querySelector(".input");
 const task_input = document.querySelector("#task-input");
 const task_list = document.querySelector(".task-list");
 
-const list_container = localStorage.getItem("list_container");
+const task = localStorage.getItem("task");
+
+if (!task) 
+{
+    // For the buttons container
+        const list_buttons = document.createElement("div");
+        list_buttons.classList.add("list-buttons");
+        // For the edit button
+        const edit_button = document.createElement("button");
+        edit_button.classList.add("button");
+        edit_button.value = "edit";
+        edit_button.innerHTML = "Edit";
+        // For the resolve button
+        const resolve_button = document.createElement("button");
+        resolve_button.classList.add("button");
+        resolve_button.value = "resolve";
+        resolve_button.innerHTML = "Resolve";
+    
+        // For the list container 
+        const list_container = document.createElement("div");
+        list_container.classList.add("list-container");
+    
+        // For the recorded tasks
+        const recorded_task = document.createElement("div");
+        recorded_task.classList.add("recorded-task");
+    
+        // To make the input text empty again
+        task_input.value = "";
+    
+        // To append the elements
+        list_buttons.appendChild(edit_button);
+        list_buttons.appendChild(resolve_button);
+        recorded_task.appendChild(listed_task);
+        list_container.appendChild(recorded_task);
+        list_container.appendChild(list_buttons);
+        task_list.appendChild(list_container);
+}
 
 input.addEventListener("submit", (e) =>
 {
@@ -10,6 +46,10 @@ input.addEventListener("submit", (e) =>
     e.preventDefault();
 
     const task = task_input.value;
+
+    localStorage.setItem("task", task);
+
+    console.log(task);
 
     if (task == "")
     {
@@ -56,10 +96,6 @@ input.addEventListener("submit", (e) =>
     list_container.appendChild(recorded_task);
     list_container.appendChild(list_buttons);
     task_list.appendChild(list_container);
-
-    localStorage.setItem("list_container", list_container);
-
-    list_container = localStorage.getItem("list_container");
 
     edit_button.addEventListener("click", () =>
     {
